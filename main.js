b7038da33c4ffa9f79d33ea6dbc74a823d75ac45
Matthew Bickell <matthew.bickell@Nology.local>
2019-10-14 10:58:36 +0100

    Started work on reset button

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-const nologists = [
+let nologists = [
   "Adam", "Aleo", "Alex", "Clara", "Dan", "Kat", "Matt", "Maya", "Sam"
 ];
+let randomNologists = [];
 
 const generateRandomNumber = array => Math.floor(Math.random() * array.length);
 
-let randomNologists = [];
+const insertNologists = (array) => {
+  $("#group-one").append(`<ul><li>${array[0]}</li><li>${array[1]}</li><li>${array[2]}</li></ul>`)
+  $("#group-two").append(`<ul><li>${array[3]}</li><li>${array[4]}</li><li>${array[5]}</li></ul>`)
+  $("#group-three").append(`<ul><li>${array[6]}</li><li>${array[7]}</li><li>${array[8]}</li></ul>`)
+}
+
+
 const generateRandomGroups = array => {
   while (array.length > 0) {
     randomNologists.push(array.splice(generateRandomNumber(array), 1));
-  }  
+  }
+  insertNologists(randomNologists);
 }
 
 generateRandomGroups(nologists);
 
-$("#group-one").append(`<ul><li>${randomNologists[0]}</li><li>${randomNologists[1]}</li><li>${randomNologists[2]}</li></ul>`)
-$("#group-two").append(`<ul><li>${randomNologists[3]}</li><li>${randomNologists[4]}</li><li>${randomNologists[5]}</li></ul>`)
-$("#group-three").append(`<ul><li>${randomNologists[6]}</li><li>${randomNologists[7]}</li><li>${randomNologists[8]}</li></ul>`)
+$("button").click( () => {
+  nologists = ["Adam", "Aleo", "Alex", "Clara", "Dan", "Kat", "Matt", "Maya", "Sam"];
+  randomNologists = [];
+  generateRandomGroups(nologists);
+})
+
