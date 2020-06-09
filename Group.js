773bae89c2a05c7ac856b8d9bfac1d84c4eeecf2
Matthew Bickell <matthew@nology.io>
2020-06-09 09:47:23 +0100

    tried something

---
+++ Group.js
@@ -0,0 +1,10 @@
+// export default class Group {
+//   constructor(number, people) {
+//     this.number = number,
+//     this.people = people
+//   }
+
+//   render: () => {
+//     return ``
+//   }
+// }
